4763fedfbbe7c3311c0adbee878e674b778b1a9e
Ryan Sinclair <sinky1903@aol.com>
2017-02-06 14:28:05 +0000

    main page loads and add new entry works

--- app.rb
+++ app.rb
@@ -3,12 +3,7 @@ require('sinatra/contrib/all')
 require('pry-byebug')
 require_relative('./models/albums')
 
-# get '/stock' do
-#  erb (:index) 
-# end
-
 get '/stock' do
- # @artists = Artist.all()
  @albums = Album.all
  erb (:index) 
 end
@@ -19,21 +14,21 @@ end
 
 post '/stock' do
   @albums = Album.new(params)
-  @album.save
+  @albums.save
   erb(:create)
 end  
 
 get '/stock/:id' do
-  @albums = Album.find(params[:id])
+  @album = Album.find(params[:id])
   erb(:show)
 end
 
 post '/stock/:id' do
-  @albums = Album.update(params)
+  @album = Album.update(params)
   redirect to ("/stock/#{params[:id]}")
 end
 
 post '/stock/:id/delete' do
-@albums = Album.destroy(params[:id])
-redirect to ('/stock')
-  end
+  @album = Album.delete(params[:id])
+  redirect to ('/stock')
+end
--- models/albums.rb
+++ models/albums.rb
@@ -28,10 +28,10 @@ class Album
   end
 
 
-  def delete()
-    sql = "DELETE * FROM album WHERE id = #{@id};"
-    SqlRunner.run(sql)
-  end
+  # def delete()
+  #   sql = "DELETE * FROM album WHERE id = #{@id};"
+  #   SqlRunner.run(sql)
+  # end
 
 
   def self.all()
@@ -42,21 +42,38 @@ class Album
   end
 
 
-  def self.delete_all
-    sql = "DELETE FROM album;"
+  def self.find(id)
+    sql = "SELECT * FROM album WHERE id=#{id};"
+    albums = SqlRunner.run(sql)
+    result = Album.new(album.first)
+    return result
+  end
+
+
+  def self.delete( id )
+    sql = "DELETE FROM album WHERE id=#{id};"
+    SqlRunner.run(sql)
+  end
+
+
+  def self.update( options )
+    sql = "UPDATE album SET title= '#{options['title']}',
+    genre= '#{options['genre']}'
+    artist_id= #{options['artist_id']}
+    quantity= #{options['quantity']} WHERE id=#{options['id']}'"
     SqlRunner.run(sql)
   end
 
 
   def artist(artist_id)
-    sql = "SELECT * FROM albums WHERE id=#{artist_id}"
+    sql = "SELECT * FROM album WHERE id=#{artist_id};"
     student = SqlRunner.run(sql)
   end
 
 
   def join
-    sql = "SELECT * FROM artists art
-          INNER JOIN albums alb
+    sql = "SELECT * FROM artist art
+          INNER JOIN album alb
           ON alb.artist_id = art.id
           WHERE art.id = #{@artist_id}"
     results = SqlRunner.run( sql )
